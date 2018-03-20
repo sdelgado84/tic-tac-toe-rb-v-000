7d0b65cd14a8b5088a7e34838950f492cb2536aa
Salvador Delgado <sdelgado84@gmail.com>
2018-03-20 03:23:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,3 +28,40 @@ def move(index, char="X")
     turn_count
     display_board
   end
+  #position_taken
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+  # valid_move
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  #Def_Turn
+  def turn
+    puts "Please enter 1-9:"
+    i = gets.strip
+    index = input_to_index(i)
+    m = valid_move?(index)
+    if m == true
+      move(index, current_player)
+    else m == false
+      until m == true
+        puts "Sorry, that was an invalid move. Please enter 1-9:"
+        display_board
+        i = gets.strip
+        index = input_to_index(i)
+        m = valid_move?(index)
+        move(index, current_player)
+      end
+    end
+  end
+#Def turn_count
+def turn_count
+    count = 0
+    @board.each do |move|
+      if move == "X" || move == "O"
+        count += 1
+      end
+    end
+    count
+  end
